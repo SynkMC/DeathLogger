fb32d6527239708cd78c1826d94618dc1422fa11
Synk_ <81869570+SynkMC@users.noreply.github.com>
2024-08-16 13:49:32 +0200

    Switched to using SynkLibs, changed API to 1.13
    Took 8 minutes

--- src/main/java/cc/synkdev/deathLogger/DeathLogger.java
+++ src/main/java/cc/synkdev/deathLogger/DeathLogger.java
@@ -7,36 +7,29 @@ import cc.synkdev.deathLogger.listener.DeathListener;
 import cc.synkdev.deathLogger.manager.Death;
 import cc.synkdev.deathLogger.manager.FileManager;
 import cc.synkdev.deathLogger.manager.Lang;
-import cc.synkdev.deathLogger.manager.UpdateManager;
+import cc.synkdev.synkLibs.Utils;
+import cc.synkdev.synkLibs.components.SynkPlugin;
 import lombok.Getter;
-import lombok.Setter;
 import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
-public final class DeathLogger extends JavaPlugin {
+public final class DeathLogger extends JavaPlugin implements SynkPlugin {
     @Getter private static DeathLogger instance;
     @Getter private final String prefix = ChatColor.translateAlternateColorCodes('&', "&8[&6DeathLogger&8] » &r");
     @Getter private FileManager fm;
     public List<Death> deaths = new ArrayList<>();
     Lang lm;
 
-    private final String version = "2.3";
-    @Setter private String newVersion = null;
-    @Getter @Setter Boolean update = false;
-
-    UpdateManager um;
 
     public void onEnable() {
         instance = this;
-        um = new UpdateManager();
-        um.checkVer();
+        new Utils(this).checkUpdate();
         lm = new Lang();
         lm.init();
         lm.load();
@@ -48,7 +41,6 @@ public final class DeathLogger extends JavaPlugin {
         this.getCommand("dl").setExecutor(new MainCommand());
         this.getCommand("dl").setTabCompleter(new MainCommand());
         Bukkit.getPluginManager().registerEvents(new DeathListener(), this);
-        Bukkit.getPluginManager().registerEvents(new UpdateManager(), this);
         int plId = 22687;
         new Metrics(this, plId);
     }
@@ -59,5 +51,25 @@ public final class DeathLogger extends JavaPlugin {
     }
     public void log(String s) {
         Bukkit.getConsoleSender().sendMessage(s);
+    }
+
+    @Override
+    public String name() {
+        return "DeathLogger";
+    }
+
+    @Override
+    public String ver() {
+        return "2.4";
+    }
+
+    @Override
+    public String dlLink() {
+        return "https://modrinth.com/plugin/deathlogger";
+    }
+
+    @Override
+    public String prefix() {
+        return prefix;
     }
 }
